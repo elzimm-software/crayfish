597cef2079251a0a4ddfa28243b4cf3a1e940afd
Elijah Zimmerman <elijahz0409@gmail.com>
2025-10-14 03:12:37 -0400

    Added random f64 utilities

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 pub use std::f64::consts::PI as fPI;
+use rand::Rng;
 
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * fPI / 180.0
@@ -6,4 +7,12 @@ pub fn degrees_to_radians(degrees: f64) -> f64 {
 
 pub fn radians_to_degrees(radians: f64) -> f64 {
     radians * 180.0 / fPI
+}
+
+pub fn rand_f64() -> f64 {
+    rand::rng().random()
+}
+
+pub fn rand_f64_in(min: f64, max: f64) -> f64 {
+    rand::rng().random_range(min..max)
 }
