453f89d0d70713ee7771808e625edd69d5b35682
Elijah Zimmerman <elijahz0409@gmail.com>
2025-10-14 05:47:13 -0400

    Implemented gamma correction

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,6 @@
-use crate::vec3::Vec3;
+use crate::utils::vec3::Vec3;
 use image::Rgb;
-use crate::interval::Interval;
+use crate::utils::interval::Interval;
 use once_cell::sync::Lazy;
 
 static INTENSITY: Lazy<Interval> = Lazy::new(|| {
@@ -9,15 +9,29 @@ static INTENSITY: Lazy<Interval> = Lazy::new(|| {
 
 pub type Color = Vec3;
 
+#[inline(always)]
 fn component_to_byte(component: f64) -> u8 {
     (256.0 * INTENSITY.clamp(component)) as u8
 }
 
+#[inline(always)]
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
+}
+
 pub fn write_color(pixel: &mut Rgb<u8>, color: Color) {
+    
+    let r = linear_to_gamma(color.x);
+    let g = linear_to_gamma(color.y);
+    let b = linear_to_gamma(color.z);
 
     *pixel = Rgb([
-        component_to_byte(color.x),
-        component_to_byte(color.y),
-        component_to_byte(color.z),
+        component_to_byte(r),
+        component_to_byte(g),
+        component_to_byte(b),
     ]);
 }
